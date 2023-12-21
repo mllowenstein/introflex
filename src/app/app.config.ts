c5d39c907d6eb69efa547559f0b4af4e6b001116
Michael Lowenstein <mlowenstein@fullstacklabs.co>
2023-12-21 06:11:26 -0700

    chore: deps extension and reorganization - some stylistic planning and layout of the app design coming out shortly & updating some more to match with client counterpart.
    :

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,34 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import {
+  ScreenTrackingService,
+  UserTrackingService,
+  getAnalytics,
+  provideAnalytics,
+} from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from '@env/environment';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideRemoteConfig(() => getRemoteConfig()),
+      provideAnalytics(() => getAnalytics()),
+      provideFirestore(() => getFirestore()),
+      provideMessaging(() => getMessaging()),
+      provideDatabase(() => getDatabase()),
+      provideStorage(() => getStorage()),
+    ]),
+    provideRouter(routes),
+    ScreenTrackingService,
+    UserTrackingService,
+  ],
 };
